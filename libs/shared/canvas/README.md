e54685499511d80b646469cc4f51c8c8441be0e0
KasunWijeratne <wijeratna.e.kasun@gmail.com>
2025-10-19 00:31:39 +0530

    react flow implemented in a shared canvas module

---
+++ libs/shared/canvas/README.md
@@ -0,0 +1,7 @@
+# @workflow-builder/canvas
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test @workflow-builder/canvas` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/shared/canvas/src/canvas.tsx
@@ -0,0 +1,38 @@
+import {
+  ReactFlow,
+  Node,
+  Edge,
+  OnNodesChange,
+  OnEdgesChange,
+  Connection,
+} from '@xyflow/react';
+import '@xyflow/react/dist/style.css';
+
+interface CanvasProps {
+  nodes: Node[];
+  edges: Edge[];
+  onNodesChange: OnNodesChange;
+  onEdgesChange: OnEdgesChange;
+  onConnect: (data: Edge | Connection) => void;
+}
+
+const Canvas = ({
+  nodes,
+  edges,
+  onNodesChange,
+  onEdgesChange,
+  onConnect,
+}: CanvasProps) => {
+  return (
+    <ReactFlow
+      nodes={nodes}
+      edges={edges}
+      onNodesChange={onNodesChange}
+      onEdgesChange={onEdgesChange}
+      onConnect={onConnect}
+      fitView
+    />
+  );
+};
+
+export default Canvas;
