8112e3d76c8e393a49fc311e5e207949a89acb09
KasunWijeratne <wijeratna.e.kasun@gmail.com>
2025-10-19 13:10:15 +0530

    new diagram page refactored to support adding new nodes from last edge

--- apps/main/src/app/diagram/new-diagram.tsx
+++ apps/main/src/app/diagram/new-diagram.tsx
@@ -1,46 +1,98 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useRef } from 'react';
 import { TopbarLayout } from '@shared/ui';
 import {
   Canvas,
-  applyNodeChanges,
-  applyEdgeChanges,
   addEdge,
+  useNodesState,
+  useEdgesState,
+  useReactFlow,
+  Edge,
+  Node,
+  Connection,
+  FinalConnectionState,
 } from '@shared/canvas';
+import {
+  CustomLabelNode,
+  nodeType as customLabelNodeType,
+} from '@shared/nodes-customlabel';
 
 const initialNodes = [
-  { id: 'n1', position: { x: 0, y: 0 }, data: { label: 'Node 1' } },
-  { id: 'n2', position: { x: 0, y: 100 }, data: { label: 'Node 2' } },
+  {
+    id: 'n1',
+    type: customLabelNodeType,
+    position: { x: 0, y: 0 },
+    data: { label: 'Node 1' },
+  },
 ];
-const initialEdges = [{ id: 'n1-n2', source: 'n1', target: 'n2' }];
+
+const nodeTypes = {
+  [customLabelNodeType]: CustomLabelNode,
+};
+
+const getId = (length: number) => `${++length}`;
+const nodeOrigin: [number, number] = [0.5, 0];
 
 const NewDiagram = () => {
-  const [nodes, setNodes] = useState(initialNodes);
-  const [edges, setEdges] = useState(initialEdges);
+  const reactFlowWrapper = useRef(null);
 
-  const onNodesChange = useCallback(
-    (changes) =>
-      setNodes((nodesSnapshot) => applyNodeChanges(changes, nodesSnapshot)),
-    []
-  );
-  const onEdgesChange = useCallback(
-    (changes) =>
-      setEdges((edgesSnapshot) => applyEdgeChanges(changes, edgesSnapshot)),
-    []
-  );
+  const [nodes, setNodes, onNodesChange] = useNodesState<Node>(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState<Edge>([]);
+  const { screenToFlowPosition } = useReactFlow();
+
   const onConnect = useCallback(
-    (params) => setEdges((edgesSnapshot) => addEdge(params, edgesSnapshot)),
-    []
+    (params: Edge | Connection) => setEdges((eds) => addEdge(params, eds)),
+    [setEdges]
+  );
+
+  const onConnectEnd = useCallback(
+    (event: MouseEvent | TouchEvent, connectionState: FinalConnectionState) => {
+      // when a connection is dropped on the pane it's not valid
+      if (!connectionState.isValid) {
+        // we need to remove the wrapper bounds, in order to get the correct position
+        const id = getId(nodes.length);
+        const { clientX, clientY } =
+          'changedTouches' in event ? event.changedTouches[0] : event;
+        const newNode: Node = {
+          id,
+          type: customLabelNodeType,
+          position: screenToFlowPosition({
+            x: clientX,
+            y: clientY,
+          }),
+          data: { label: `Node ${id}` },
+          origin: [0.5, 0.0],
+        };
+
+        setNodes((nds) => nds.concat(newNode));
+        setEdges((eds) =>
+          connectionState.fromNode
+            ? eds.concat({
+                id,
+                source: connectionState.fromNode.id,
+                target: id,
+              })
+            : eds
+        );
+      }
+    },
+    [nodes.length, screenToFlowPosition, setEdges, setNodes]
   );
 
   return (
     <TopbarLayout>
-      <div style={{ width: '100%', height: '100%' }}>
+      <div style={{ width: '100%', height: '100%' }} ref={reactFlowWrapper}>
         <Canvas
+          fitView
           nodes={nodes}
           edges={edges}
           onNodesChange={onNodesChange}
           onEdgesChange={onEdgesChange}
           onConnect={onConnect}
+          onConnectEnd={onConnectEnd}
+          fitViewOptions={{ padding: 2 }}
+          nodeOrigin={nodeOrigin}
+          nodeTypes={nodeTypes}
+          style={{ backgroundColor: '#eee' }}
         />
       </div>
     </TopbarLayout>
