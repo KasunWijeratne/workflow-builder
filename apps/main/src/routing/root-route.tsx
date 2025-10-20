d931ed3c535e953a0ad3f1eb59085b1206ebd1d6
KasunWijeratne <wijeratna.e.kasun@gmail.com>
2025-10-20 20:22:12 +0530

    root route redirection refactored

--- apps/main/src/routing/root-route.tsx
+++ apps/main/src/routing/root-route.tsx
@@ -1,12 +1,18 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useAuth } from '@shared/auth';
 
 const RootRoute = () => {
   const navigate = useNavigate();
+  const { user, loading } = useAuth();
 
   useEffect(() => {
-    navigate('/dashboard');
-  }, [navigate]);
+    if (!loading && user) {
+      navigate('/dashboard');
+    } else {
+      navigate('/login');
+    }
+  }, [navigate, user, loading]);
 
   return null;
 };
