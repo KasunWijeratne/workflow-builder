57c46a9c01ab4dbaa6f1d7ab3acfb94a1bd7562a
KasunWijeratne <wijeratna.e.kasun@gmail.com>
2025-10-20 13:28:58 +0530

    theme custom variables added with types, topbar layout styling

--- libs/shared/ui/src/layouts/topbar.layout.tsx
+++ libs/shared/ui/src/layouts/topbar.layout.tsx
@@ -2,6 +2,7 @@ import { FC, PropsWithChildren } from 'react';
 import { Box } from '../components/Box';
 import { Stack } from '../components/Stack';
 import { Link } from 'react-router-dom';
+import { Typography } from '@mui/material';
 
 interface TopbarLayoutProps extends PropsWithChildren {
   userMenu?: React.ReactNode;
@@ -14,14 +15,26 @@ export const TopbarLayout: FC<TopbarLayoutProps> = ({ children, userMenu }) => {
         direction="row"
         justifyContent="space-between"
         alignItems="center"
-        sx={{ height: 40 }}
+        sx={{
+          height: 60,
+          p: 2,
+          borderBottom: '1px solid',
+          borderBottomColor: 'border.main',
+        }}
       >
-        <h2 style={{ margin: 0 }}>
-          <Link to={'/dashboard'}>Diagram Builder</Link>
-        </h2>
+        <Typography variant="h2" style={{ margin: 0, fontSize: '2rem' }}>
+          <Link to={'/dashboard'} style={{ textDecoration: 'none' }}>
+            Diagram Builder
+          </Link>
+        </Typography>
         {userMenu && userMenu}
       </Stack>
-      <Box flex={1}>{children}</Box>
+      <Box
+        flex={1}
+        sx={{ p: 2, width: '100%', maxWidth: 1500, margin: '0 auto' }}
+      >
+        {children}
+      </Box>
     </Stack>
   );
 };
